2d6ea368a0b2370c6d4625073cf0aaccfefed822
Olivia Todesco <104026140+SrtaLiv@users.noreply.github.com>
2024-07-09 21:45:55 -0300

    Funcionan imagenes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, FormsModule, NgOptimizedImage],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, FormsModule, NgOptimizedImage, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
