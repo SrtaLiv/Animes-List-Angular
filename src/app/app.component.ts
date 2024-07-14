4e30b0fe4c1832104a349f6cf6cb7ae5b3f2f6ab
Olivia Todesco <104026140+SrtaLiv@users.noreply.github.com>
2024-07-14 00:33:48 -0300

    seguir en la netbook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,12 @@ import { CarouselComponent } from './pages/carousel/carousel.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { NavComponent } from "./pages/nav/nav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, FormsModule, NgOptimizedImage, FooterComponent, CarouselComponent, FontAwesomeModule],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, FormsModule, NgOptimizedImage, FooterComponent, CarouselComponent, FontAwesomeModule, NavComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
